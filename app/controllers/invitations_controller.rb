a495cd3ab3ce38ad5e8ac9b747b2f894f2452c96
Brent Barnes <BrenttBarness@gmail.com>
2022-08-02 11:33:27 -0400

    can now send invitations. checks if invitation exists

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,6 @@
 class InvitationsController < ApplicationController
-  before_action :set_event, only: [:create]
   before_action :set_user, only: [:create]
+  before_action :set_event, only: [:create]
   before_action :check_if_invitation_exists, only: [:create]
 
   def new
@@ -8,16 +8,11 @@ class InvitationsController < ApplicationController
   end
 
   def create
-    ip = invitation_params
-    found_user = Invitation.get_invited_user(params[:invitation][:invited_user_email])
-    ip[:invited_user_id] = found_user.id
-
-    @invitation = found_user.invitations.find_or_create_by(invitation_params)
+    @invitation = @user.invitations.build(invited_user_id: @user.id, invited_event_id: @event.id)
     if @invitation.save
-      flash[:notice] = "Your invitation has been successfully sent"
-      redirect_to new_invitation_path(invited_event_id: found_user.id)
+      flash[:notice] = "Your invitation has successfully been sent"
+      redirect_to new_invitation_path(invitation: {invited_event_id: @event.id})
     else
-      ip[:invited_event_id] = found_user.id
       render :action => "new", status: :unprocessable_entity
     end
   end
@@ -29,27 +24,20 @@ class InvitationsController < ApplicationController
   end
 
   def set_event
-    @event = Event.find(params[:invited_event_id])
+    @event = Event.find(params[:invitation][:invited_event_id])
   end
 
   def set_user
-    @user = User.find_by(email: params[:invited_user_email])
+    @user = User.find_by(email: params[:invitation][:invited_user_email])
   end
 
   def check_if_invitation_exists
-    found_invitation = Invitation.find_by_invited_user_email(params[:invitation][:invited_user_email])
-    binding.pry
-    if found_invitation.nil?
-      flash.now[:error] = "There is no user with this email."
-      render "new", status: :unprocessable_entity 
-    elsif Invitation.exists?(email: found_invitation.invited_user_email)
-      flash.now[:error] = "User exists."
-      #                       invited_user_id: found_invitation.invited_user_id,
-      #                       invited_event_id: found_invitation.invited_event_id)
-      #   flash.now[:error] = "This person is already invited."
-      #   render "new", status: :unprocessable_entity
+    if @user.nil?
+      flash[:notice] = "There is no user with this email."
+      redirect_to new_invitation_path(invitation: {invited_event_id: @event.id})
+    elsif Invitation.where(invited_event_id: @event.id, invited_user_id: @user.id).exists?
+      flash[:notice] = "This user has already received an invite."
+      redirect_to new_invitation_path(invitation: {invited_event_id: @event.id})
     end
-
-
   end
 end
